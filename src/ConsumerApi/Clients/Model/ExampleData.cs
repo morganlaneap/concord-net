633ad8edb8cb0559167f645026951fb1e3e20541
Richard Dunning <dunning_r@hotmail.co.uk>
2020-05-17 06:29:58 +0100

    Add example consumer api

---
+++ src/ConsumerApi/Clients/Model/ExampleData.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsumerApi.Clients.Model
+{
+    public class ExampleData
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        
+        [JsonPropertyName("color")]
+        public string Color { get; set; }
+    }
+}
